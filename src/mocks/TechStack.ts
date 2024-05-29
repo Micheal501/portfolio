79bcececb7064741f0d2d7e8781b6209210ff965
Micheal Owen <mowen8983@gmail.com>
2024-05-29 16:03:02 +0700

    feat: adding second projects

--- src/mocks/TechStack.ts
+++ src/mocks/TechStack.ts
@@ -1,6 +1,7 @@
 const TechStack = [
   {
     title: "Portfolio V1",
+    image: "../../assets/img/portfolio-v1.png",
     description:
       "This is my first portfolio website. I make this to apply my skills that i got before from my learning progress",
     value: "portfolio-v1",
@@ -10,6 +11,24 @@ const TechStack = [
       },
       {
         tech: "Tailwind",
+      },
+    ],
+  },
+  {
+    title: "anikyun",
+    image: "../../assets/img/kny.png",
+    description:
+      "Anime website for searching recommendation anime and top anime of all time",
+    value: "anikyun",
+    techstack: [
+      {
+        tech: "React",
+      },
+      {
+        tech: "Mantine",
+      },
+      {
+        tech: "Axios",
       },
     ],
   },
